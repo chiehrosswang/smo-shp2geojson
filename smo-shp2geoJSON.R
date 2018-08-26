69f97a0f6b41c30ec5f391c0fe9676e1067ca5d2
chiehrosswang <cwang.gatech@gmail.com>
2018-08-25 22:23:35 -0400

    Add comments to R code

--- smo-shp2geoJSON.R
+++ smo-shp2geoJSON.R
@@ -4,17 +4,18 @@ library(geojsonio)
 
 # Load shapefile
 roads <- readOGR('./Data/road_status.shp', 'road_status')
-summary(roads)
+summary(roads) #proj4string:[+proj=utm +zone=17 +datum=NAD83 +units=m ...
 
+# Project layer to GCS (latlng)
 roads.latlng <- spTransform(roads, CRS("+proj=longlat"))
-summary(roads.latlng)
+summary(roads.latlng) #proj4string:[+proj=longlat +ellps=WGS84]
 
 # Create a layer with active on Florida State 
 # Highway System roads (ROAD_STATU == "02")
 roads.latlng_02 <- roads.latlng %>%
   filter(ROAD_STATU == "02")
-road_status_02 <- geojson_json(roads.latlng_02)
-geojson_write(road_status_02, file = "./road_status_02.geojson")
+road_status_02 <- geojson_json(roads.latlng_02) # convert shp to GeoJSON
+geojson_write(road_status_02, file = "./road_status_02.geojson") # write file
 
 # Create a layer with active on SHS ("02"), active 
 # exclusive ("07"), and active off SHS ("09") roads
@@ -22,5 +23,5 @@ roads.latlng_02_07_09 <- roads.latlng %>%
   filter(ROAD_STATU == "02" | 
            ROAD_STATU == "07" | 
            ROAD_STATU == "09")
-road_status_02_07_09 <- geojson_json(roads.latlng_02_07_09)
-geojson_write(road_status_02_07_09, file = "./road_status_02_07_09.geojson")
+road_status_02_07_09 <- geojson_json(roads.latlng_02_07_09) # convert shp to GeoJSON
+geojson_write(road_status_02_07_09, file = "./road_status_02_07_09.geojson") # write file
