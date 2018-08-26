8782e0f6d39a4778add94c2f8a1bdd00142c9190
chiehrosswang <cwang.gatech@gmail.com>
2018-08-26 00:43:14 -0400

    Update folders and coordinate precision

--- smo-shp2geoJSON.R
+++ smo-shp2geoJSON.R
@@ -14,8 +14,8 @@ summary(roads.latlng) #proj4string:[+proj=longlat +ellps=WGS84]
 # Highway System roads (ROAD_STATU == "02")
 roads.latlng_02 <- roads.latlng %>%
   filter(ROAD_STATU == "02")
-road_status_02 <- geojson_json(roads.latlng_02, digits = 8) # convert shp to GeoJSON
-geojson_write(road_status_02, file = "./road_status_02.geojson") # write file
+road_status_02 <- geojson_json(roads.latlng_02, digits = 15) # convert shp to GeoJSON
+geojson_write(road_status_02, file = "./GeoJSON/road_status_02.geojson", digits = 15) # write file
 
 # Create a layer with active on SHS ("02"), active 
 # exclusive ("07"), and active off SHS ("09") roads
@@ -23,5 +23,5 @@ roads.latlng_02_07_09 <- roads.latlng %>%
   filter(ROAD_STATU == "02" | 
            ROAD_STATU == "07" | 
            ROAD_STATU == "09")
-road_status_02_07_09 <- geojson_json(roads.latlng_02_07_09, digits = 8) # convert shp to GeoJSON
-geojson_write(road_status_02_07_09, file = "./road_status_02_07_09.geojson") # write file
+road_status_02_07_09 <- geojson_json(roads.latlng_02_07_09, digits = 15) # convert shp to GeoJSON
+geojson_write(road_status_02_07_09, file = "./GeoJSON/road_status_02_07_09.geojson", digits = 15) # write file
